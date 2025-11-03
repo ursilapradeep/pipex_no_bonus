51857ff457b9e226ab6ae376cfd05ac61202fb1c
Ursilapradeep Vadakkumpuram <uvadakku@3-E-20.42heilbronn.de>
2025-11-03 10:54:36 +0100

    added files to libft

---
+++ libft_/ft_putendl_fd.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uvadakku <uvadakku@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 11:04:16 by uvadakku          #+#    #+#             */
+/*   Updated: 2025/07/23 15:35:55 by uvadakku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
+#include <string.h>
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!s || fd < 0)
+		return ;
+	while (s[i])
+	{
+		write (fd, &s[i], 1);
+		i++;
+	}
+	write (fd, "\n", 1);
+}
